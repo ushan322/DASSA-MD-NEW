f7e3a021879805a28bffa215ecd77095c770a158
ushan322 <sadeeplakshan209@gmail.com>
2025-04-07 07:25:01 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -17,7 +17,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false,
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "private",
-  OWNER_NAME: process.env.OWNER_NAME || "DASSA MD",
+  OWNER_NAME: process.env.OWNER_NAME || "UVI Z MD",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "94743277981",
   GEMINI_KEY: process.env.GEMINI_KEY || "",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false,
